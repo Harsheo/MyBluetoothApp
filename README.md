462626548dc9f664ad68631f65a569bb1f65b273
Harsheo <harsh.harsheo@gmail.com>
2021-09-13 11:07:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyBluetoothApp
+ An android simple wearOS app to display the message on screen sent by the mobile device.
---
+++ wear/src/main/java/com/example/mybluetoothapp/MessageActivity.java
@@ -0,0 +1,48 @@
+package com.example.mybluetoothapp;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MessageActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        setAmbientEnabled();
+
+        // Register the local broadcast receiver
+        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
+        MessageReceiver messageReceiver = new MessageReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("message");
+            Log.v("myTag", "Main activity received message: " + message);
+            // Display message in UI
+            mTextView.setText(message);
+        }
+    }
+}
